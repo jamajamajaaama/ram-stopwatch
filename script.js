2bd30296d4ec97c6d4d3d8540faae408380c1dae
jamajamajaaama <jam.ikdiena@gmail.com>
2023-06-09 17:39:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ function main() {
 
         $(this).attr("disabled", true);
         $("#button-start").attr("disabled", false);
-        $("#button-start").val("CONTINUE");
+        $("#button-start").text("CONTINUE");
         $("#button-rollback").attr("disabled", false);
 
         let duration = stopwatch.getLastLaunch().duration;
